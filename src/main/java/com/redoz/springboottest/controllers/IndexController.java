58aae633b988c828c116cd453fa295246e7dd755
Nicolas Olmos <yohannicolas2003@gmail.com>
2022-04-12 13:35:45 -0500

    Add a car object into index

--- src/main/java/com/redoz/springboottest/controllers/IndexController.java
+++ src/main/java/com/redoz/springboottest/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.redoz.springboottest.controllers;
 
+import com.redoz.springboottest.models.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, Car car){
+        car.setLicensePlate("ABC123");
+        car.setBrand("Ford");
+        car.setName("Explorer");
+        car.setColor("Black");
 
+        model.addAttribute("car",car);
         return "index";
     }
 }
